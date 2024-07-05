f039fd5decb02e794c815db28edfae693a950684
fv-apeiroo <fv@apeiroo.com>
2024-07-05 08:02:06 +0000

    update: update index to avoid cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import dutiesRoutes from './routes/duties';
-import path from 'path';
+import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import dutiesRoutes from './routes/duties'
+import path from 'path'
+import cors from 'cors'
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.use(bodyParser.json());
+app.use(cors(
+    {origin: 'http://localhost:8081'}
+))
 
-app.use('/api/duties', dutiesRoutes);
+app.use(bodyParser.json())
+
+app.use('/api/duties', dutiesRoutes)
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+    console.log(`Server running on port ${port}`)
+})
